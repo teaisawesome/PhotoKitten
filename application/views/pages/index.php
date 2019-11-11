f600bec4bcc0ca9d700b76004b9abd9c691a14af
Robi <dekany.r123@gmail.com>
2019-11-11 13:33:57 +0100

    test-features commit

--- application/views/pages/index.php
+++ application/views/pages/index.php
@@ -1,22 +1,41 @@
-<nav class="navbar navbar-expand-lg navbar-dark">
-  <a class="navbar-brand" href="#">PhotoKitten<img src='<?php echo base_url()."assets/img/brand.png";?>'/></a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Features</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Pricing</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-      </li>
-    </ul>
+<div class="container-fluid front-view">
+  <nav class="navbar navbar-expand-lg navbar-dark">
+      <a class="navbar-brand" href="#">PhotoKitten<img src='<?php echo base_url()."assets/img/brand.png";?>'/></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Features</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Pricing</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+          </li>
+        </ul>
+      </div>
+  </nav>
+</div>
+<section class="jumbotron">
+  <h1 class="display-4 text-center">Hello World!<h1>
+  <p class="lead text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae ab obcaecati iusto distinctio amet totam aut dolorem ullam et ducimus repudiandae maiores repellendus quasi atque, doloremque cupiditate fugiat reiciendis deleniti.</p>
+  <hr class="my-4">
+</section>
+<div class="container-fluid">
+  <div class="row featurette">
+    <div class="col-md-5 text-center">
+      <img class="img-fluid mx-auto featurette-image" src="<?php echo base_url()."assets/img/section-img2.jpg";?>">
+    </div>
+    <div class="col-md-5 text-center">
+      <h2>Lorem ipsum</h2>
+      <hr>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium id nulla libero, fugit perspiciatis ad porro beatae adipisci commodi blanditiis ut nobis veniam aliquam quas obcaecati sequi, explicabo neque quo.</p>
+    </div>
   </div>
-</nav>
+</div>
